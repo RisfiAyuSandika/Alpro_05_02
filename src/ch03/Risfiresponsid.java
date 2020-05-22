a8223ba443a6b67655bf77d26017e2efcc03a20d
RisfiAyuSandika <11950125205@students.uin-suska.ac.id>
2020-05-22 22:27:15 +0700

    commit pertama

---
+++ src/ch03/Risfiresponsid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Risfiresponsid {
+    public static void main(String[] args) {
+        Scanner ras = new Scanner (System.in);
+        System.out.println("Masukkan Angka");
+        int angka = ras.nextInt();
+        int bilangan = 0;
+        for (int i = 1; i <= angka; i++){
+             if(angka%i == 0)
+             bilangan = bilangan + 1; }
+        if(bilangan == 2)
+            System.out.println(angka +  "adalah Bilangan Prima " );
+        else
+            System.out.println(angka +  "adalah Bukan Bilangan Prima " );
+
+    }
+}
