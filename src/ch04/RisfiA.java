79d2e68a3fa221d0b19961f7e640cb7d128eac8a
RisfiAyuSandika <11950125205@students.uin-suska.ac.id>
2020-05-22 22:41:25 +0700

    commit pertama

---
+++ src/ch04/RisfiA.java
@@ -0,0 +1,8 @@
+public class RisfiA {
+        public static void main(String[] args) {
+        int[] angka = {82,12,41,38,19,26,9,48,20,55,8,32,3};
+        for(int x : angka){
+            System.out.print(x+"-");
+        }
+    }
+}
---
+++ src/ch06/Bubblesort.java
@@ -0,0 +1,25 @@
+public class Bubblesort {
+    static void Bubblesort(int[] risfi) {
+        for (int r = 0; r < risfi.length; r++) {
+            for (int a = 0; a <risfi.length - 1; a++) {
+                if (risfi[a] > risfi[a + 1]) {
+                    int ayam = risfi[a];
+                    risfi[a] = risfi[a + 1];
+                    risfi[a + 1] = ayam;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] risfi = {82, 12, 41, 38, 19, 26, 9, 48, 20, 55, 8, 32, 3};
+
+        for (int r = 0; r < risfi.length; r++) {
+            System.out.print(risfi[r] + ", ");
+        }
+        System.out.println("");
+        Bubblesort(risfi);
+        for (int r = 0; r < risfi.length; r++) {
+            System.out.print(risfi[r] + ", ");
+        }
+    }
+}
